4c3abdd22229d2a273a84fe844fe144ec20f9016
Nyaughh <aarinarora02@gmail.com>
2024-09-27 12:57:06 +0530

    done

--- program1.js
+++ program1.js
@@ -1,8 +1,25 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0) return 0;
 
+  let islandCount = 0;
 
-  // write your code here
+  function markIsland(i, j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] !== 'L') return;
+    
+    grid[i][j] = 'W';
+    [[i-1, j], [i+1, j], [i, j-1], [i, j+1]].forEach(([x, y]) => markIsland(x, y));
+  }
 
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 'L') {
+        islandCount++;
+        markIsland(i, j);
+      }
+    }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,14 @@
 const decodeTheRing = function (s, p) {
+  if (s === '' && p === '') return true;
+  if (p === '') return false;
 
-    // write your code here
+  const firstMatch = s !== '' && (p[0] === '?' || p[0] === s[0]);
 
-  };
-  
-  module.exports = decodeTheRing;
+  if (p[0] === '*') {
+    return decodeTheRing(s, p.slice(1)) || (s !== '' && decodeTheRing(s.slice(1), p));
+  } else {
+    return firstMatch && decodeTheRing(s.slice(1), p.slice(1));
+  }
+};
+
+module.exports = decodeTheRing;
